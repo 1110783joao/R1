d5ae218ec580930c65ff3cb8849b8f4ee438f713
João <Joo@Joao-PC>
2013-03-08 14:58:24 +0000

    Criacao do projeto

--- 1Teste/src/pkg1teste/Main.java
+++ 1Teste/src/pkg1teste/Main.java
@@ -1,18 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg1teste;
 
-/**
- *
- * @author João
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Primeira alteracao");
         System.out.println("EASY!");
