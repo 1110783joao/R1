2a275d37e69f6d0ead051783ff4f3a9cdf290788
João <Joo@Joao-PC>
2013-03-08 15:41:13 +0000

    Criacao do projeto

--- 1Teste/src/pkg1teste/Main.java
+++ 1Teste/src/pkg1teste/Main.java
@@ -3,7 +3,7 @@ package pkg1teste;
 public class Main {
 
     public static void main(String[] args) {
-        Escola Se = new Escola("Escola da Se Lamego", 44);
+        Escola Se = new Escola("Escola Basica e Secundaria 2,3 da Se Lamego", 44);
         Aluno a1 = new Aluno("Fabio", 10, Se);
         System.out.println("\n" + Se);
         System.out.println("\n" + a1);
