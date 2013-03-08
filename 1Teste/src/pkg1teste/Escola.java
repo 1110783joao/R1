573fe16894484660d2ce50bc24e893d0a1d2a4c7
João <Joo@Joao-PC>
2013-03-08 15:04:34 +0000

    Criacao do projeto

---
+++ 1Teste/src/pkg1teste/Escola.java
@@ -0,0 +1,27 @@
+package pkg1teste;
+
+public class Escola {
+    private String nome;
+    private int numSalas;
+    public Escola(String _n, int _nS){
+        nome=_n;
+        numSalas=_nS;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumSalas() {
+        return numSalas;
+    }
+
+    public void setNumSalas(int numSalas) {
+        this.numSalas = numSalas;
+    }
+    
+}
