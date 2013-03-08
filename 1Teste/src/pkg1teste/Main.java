a931422fd36dc09cb68e5643b216d7cf501d92b4
João <Joo@Joao-PC>
2013-03-08 15:44:51 +0000

    Criacao do projeto

--- 1Teste/src/pkg1teste/Main.java
+++ 1Teste/src/pkg1teste/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         Escola Se = new Escola("Escola Basica e Secundaria 2,3 da Se Lamego", 44);
-        Aluno a1 = new Aluno("Fabio", 10, Se);
+        Escola isep = new Escola("ISEP", 444);
         System.out.println("\n" + Se);
+        System.out.println("\n" + isep);
+        
+        Aluno a1 = new Aluno("Fabio", 10, Se);
         System.out.println("\n" + a1);
+        
+        Aluno a2 = new Aluno("Joao", 69, Se);
+        System.out.println("\n" + a2);
+        
+        Aluno a3 = new Aluno("Joao Costa", 19, isep);
+        System.out.println("\n" + a3);
     }
 }
