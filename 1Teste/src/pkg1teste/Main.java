eb149655dfbe2809fc2fc086f78d4b3e8dc387eb
João <Joo@Joao-PC>
2013-03-08 15:28:16 +0000

    Criacao do projeto

--- 1Teste/src/pkg1teste/Main.java
+++ 1Teste/src/pkg1teste/Main.java
@@ -6,7 +6,8 @@ public class Main {
         System.out.println("Primeira alteracao");
         System.out.println("EASY!");
         
-        Aluno a1 = new Aluno("Fabio", 10);
+        Escola Se = new Escola("Escola da Se Lamego", 44);
+        Aluno a1 = new Aluno("Fabio", 10, Se);
         System.out.println(a1);
     }
 }
